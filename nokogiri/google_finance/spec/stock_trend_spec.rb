a5ac329e987b45affea8b9a3dd5832db04379a0d
Pratik Shah <pratik.t.shah@gmail.com>
2016-03-10 11:08:24 -0600

    Return html nokogiri objects back

--- nokogiri/google_finance/spec/stock_trend_spec.rb
+++ nokogiri/google_finance/spec/stock_trend_spec.rb
@@ -4,10 +4,29 @@ describe StockTrend do
 
   let(:s) { StockTrend.new }
 
-  # check google finance url
-  it 'should have the google url' do
-    webpage = 'https://www.google.com/finance?hl=en&ei=DongVvm_MdOS2AaU9JbAAQ'
-    expect(s.url).to eq(webpage)
+  describe 'variables' do
+    # check google finance url
+    it 'should have the google url' do
+      webpage = 'https://www.google.com/finance?hl=en&ei=DongVvm_MdOS2AaU9JbAAQ'
+      expect(s.url).to eq(webpage)
+    end
+
+    # check google finance url
+    it 'should have the goolge target ID' do
+      id = '#topmovers'
+      expect(s.trend_id).to eq(id)
+    end
+
+    # check data array to be empty
+    it 'should have an empty array for data' do
+      expect(s.data.length).to be(0)
+    end
   end
 
+  pending '#html' do
+    it 'returns a Nokogiri object back' do
+      html = s.html
+      expect(html).to be_instance_of(Nokogiri)
+    end
+  end
 end
