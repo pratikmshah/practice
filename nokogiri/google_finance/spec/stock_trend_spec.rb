bace3f0c66f3000da3c8f81e5c8c2e8a43447956
Pratik Shah <pratik.t.shah@gmail.com>
2016-03-10 14:32:26 -0600

    Remove 'Popular searches...' from data and add headers constant'

--- nokogiri/google_finance/spec/stock_trend_spec.rb
+++ nokogiri/google_finance/spec/stock_trend_spec.rb
@@ -3,6 +3,8 @@ require '../stock_trend'
 describe StockTrend do
 
   let(:s) { StockTrend.new }
+  let(:html) { s.html }
+  let(:data) { html.css(s.trend_id) }
 
   describe 'variables' do
     # check google finance url
@@ -18,15 +20,28 @@ describe StockTrend do
     end
 
     # check data array to be empty
-    it 'should have an empty array for data' do
-      expect(s.data.length).to be(0)
+    # it 'should have an empty array for data' do
+    #   expect(s.data.length).to be(0)
+    # end
+  end
+
+  describe '#html' do
+    it 'returns a Nokogiri object back' do
+      expect(html.text).to be_instance_of(String)
     end
   end
 
-  pending '#html' do
-    it 'returns a Nokogiri object back' do
-      html = s.html
-      expect(html).to be_instance_of(Nokogiri)
+  describe '#parse_trend' do
+    it 'returns html back' do
+      result = data[0].first[1]
+      expect(result).to be_instance_of(String)
+    end
+  end
+
+  describe '#convert_obj_to_text' do
+    it 'returns back an array' do
+      arr = s.convert_obj_to_text(data)
+      expect(arr.empty?).to be false
     end
   end
 end
