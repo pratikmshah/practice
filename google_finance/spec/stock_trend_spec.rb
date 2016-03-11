f7f3bd8772c51644172d18ddcd1b2155c6920f5f
Pratik Shah <pratik.t.shah@gmail.com>
2016-03-11 14:51:23 -0600

    Rspec tests pass

--- google_finance/spec/stock_trend_spec.rb
+++ google_finance/spec/stock_trend_spec.rb
@@ -9,7 +9,7 @@ describe StockTrend do
   let(:data_f) { s.format_text(txt_data) }
   let(:data_with_head) { s.insert_headers(s.remove_new_lines(data_f)) }
 
-  xdescribe 'variables' do
+  describe 'variables' do
     # check google finance url
     it 'should have the google url' do
       webpage = 'https://www.google.com/finance?hl=en&ei=DongVvm_MdOS2AaU9JbAAQ'
@@ -29,27 +29,27 @@ describe StockTrend do
     end
   end
 
-  xdescribe '#html' do
+  describe '#html' do
     it 'returns a Nokogiri object back' do
       expect(html.text).to be_instance_of(String)
     end
   end
 
-  xdescribe '#parse_trend' do
+  describe '#parse_trend' do
     it 'returns html back' do
       result = data[0].first[1]
       expect(result).to be_instance_of(String)
     end
   end
 
-  xdescribe '#convert_obj_to_text' do
+  describe '#convert_obj_to_text' do
     it 'returns back an array' do
       arr = s.convert_obj_to_text(data)
       expect(arr.empty?).to be false
     end
   end
 
-  xdescribe '#format_text' do
+  describe '#format_text' do
     it 'formats data by including headers and remove unecessary text' do
       header = [ 'Gainers', 'Losers', 'Leaders']
       arr = s.format_text(txt_data)
@@ -60,14 +60,14 @@ describe StockTrend do
     end
   end
 
-  xdescribe '#remove_new_lines' do
+  describe '#remove_new_lines' do
     it 'should not have any new line escape characters' do
       arr = s.remove_new_lines(txt_data)
       expect(arr.include?('\n')).to be(false)
     end
   end
 
-  xdescribe '#insert_headers' do
+  describe '#insert_headers' do
     it 'should have formated headers starting with T-Sym ending with Mkt Cap' do
       header = data_with_head.slice(0..3)
       expect(header).to eq( ['T-Sym', 'Gainers', 'Change', 'Mkt Cap'] )
