e21ea372ad07f42dc3b77aac16d73b8ca4bfeeda
Pratik Shah <pratik.t.shah@gmail.com>
2016-03-16 12:25:08 -0500

    Add boostrap to rails and design homepage

--- pintrested/config/routes.rb
+++ pintrested/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
   root 'home#index' # reroute index page to controller home/index page
+  get 'home/index'
+  get 'home/about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
