c5809643ddd3e222394c776122539b86ed28dbad
Pratik Shah <pratik.t.shah@gmail.com>
2016-03-25 14:20:10 -0500

    style articles listing page

--- alpha-blog/app/models/article.rb
+++ alpha-blog/app/models/article.rb
@@ -4,6 +4,6 @@ class Article < ActiveRecord::Base
   validates :title, presence: true,
             length: { minimum: 3, maximum: 50 }
   validates :description, presence: true,
-            length: { minimum: 10, maximum: 300 }
+            length: { minimum: 10 }
 
 end
