09aaf197a6b1e4ebf0e0ebcfa52a58d425f64c47
Pratik Shah <pratik.t.shah@gmail.com>
2016-03-25 15:16:23 -0500

    create user model and migration

--- alpha-blog/db/schema.rb
+++ alpha-blog/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324202000) do
+ActiveRecord::Schema.define(version: 20160325201313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20160324202000) do
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
