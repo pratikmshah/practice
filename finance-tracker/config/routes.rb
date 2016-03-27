222b4f2a0f64b460f360335d34a5b9384946790b
Pratik Shah <pratik.t.shah@gmail.com>
2016-03-27 13:37:34 -0500

    Create Stock model and methods

--- finance-tracker/config/routes.rb
+++ finance-tracker/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'welcome#index'
   devise_for :users # all routes for devise
   get 'welcome', to: 'welcome#index'
+  get 'my_portfolio', to: "users#my_portfolio"
 end
