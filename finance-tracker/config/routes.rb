7cacf75a1bed58c183f99f4b442c74e884420059
Pratik Shah <pratik.t.shah@gmail.com>
2016-03-27 14:01:09 -0500

    Add stock search bar

--- finance-tracker/config/routes.rb
+++ finance-tracker/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
   devise_for :users # all routes for devise
+
+  root 'welcome#index'
   get 'welcome', to: 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
+  get 'search_stocks', to: "stocks#search"
 end
