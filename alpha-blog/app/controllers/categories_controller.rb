9edfbfda302fa2154ca4e53471934ef9738fa43c
Pratik Shah <pratik.t.shah@gmail.com>
2016-03-26 22:40:41 -0500

    Add category feature

--- alpha-blog/app/controllers/categories_controller.rb
+++ alpha-blog/app/controllers/categories_controller.rb
@@ -20,6 +20,21 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+
+    if @category.update(category_params)
+      flash[:success] = "Category was successfully updated"
+      redirect_to category_path(@category)
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
@@ -33,7 +48,7 @@ class CategoriesController < ApplicationController
 
     def require_admin
       # you must be logged in and an admin
-      if !logged_in? || (logged_in and !current_user.admin?)
+      if !logged_in? || (logged_in? and !current_user.admin?)
         flash[:danger] = "Only admins can perform this action"
         redirect_to categories_path
       end
