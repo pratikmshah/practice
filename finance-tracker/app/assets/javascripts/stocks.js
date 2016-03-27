7b913abe23c8ae7bc53096f1ec6eba43e8bb2fc7
Pratik Shah <pratik.t.shah@gmail.com>
2016-03-27 14:30:10 -0500

    Add ajax capability to UI

--- finance-tracker/app/assets/javascripts/stocks.js
+++ finance-tracker/app/assets/javascripts/stocks.js
@@ -1,12 +1,26 @@
 var init_stock_lookup;
 
 init_stock_lookup = function() {
+
+  // show the spinner before ajax results
+  $('#stock-lookup-form').on('ajax:before', function(event, data, status){
+    show_spinner();
+  });
+
+  // stop spinner after ajax results
+  $('#stock-lookup-form').on('ajax:after', function(event, data, status){
+    hide_spinner();
+  });
+
+  // stock search found
   $('#stock-lookup-form').on('ajax:success', function(event, data, status){
     $('#stock-lookup').replaceWith(data);
     init_stock_lookup();
   });
 
+  // no stock found in search
   $('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error){
+    hide_spinner();
     $('#stock-lookup-results').replaceWith(' ');
     $('#stock-lookup-errors').replaceWith('Stock was not found.');
   });
