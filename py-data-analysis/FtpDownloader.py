204e0664af3a4a3ed8458e455bafc0159745663b
Pratik Shah <pratik.t.shah@gmail.com>
2016-05-21 17:02:04 -0500

    Download isd-lite.. file from server

--- py-data-analysis/FtpDownloader.py
+++ py-data-analysis/FtpDownloader.py
@@ -5,8 +5,14 @@ Created on Sat May 21 16:43:47 2016
 @author: Pratik
 """
 from ftplib import FTP
+import os
 
+# login and download file from ftp site
 def ftpDownloader(host, user, passwd):
-    ftp = FTP(host)
-    ftp.login(user, passwd)
-    print(ftp.nlst())
+    ftp = FTP(host)                 # get the host url of ftp site
+    ftp.login(user, passwd)         # login with username and password
+    ftp.cwd('Data')                 # change directory to Data
+    os.chdir("/Users/Pratik/Documents/Pratik/Work/practice/py-data-analysis") # change directory
+    print(ftp.nlst())               # print list of all files in dir
+    with open("isd-lite-format.pdf", 'wb') as file:     # open file and w/r
+        ftp.retrbinary('RETR isd-lite-format.pdf', file.write) # read contents of pdf and write to our file
