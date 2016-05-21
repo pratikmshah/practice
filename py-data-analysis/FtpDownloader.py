e458f4df56c47ea24f89386c40cb9e2e42b8f74b
Pratik Shah <pratik.t.shah@gmail.com>
2016-05-21 17:44:32 -0500

    FTP function to download multiple files

--- py-data-analysis/FtpDownloader.py
+++ py-data-analysis/FtpDownloader.py
@@ -1,18 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat May 21 16:43:47 2016
+Created on Sat May 21 17:09:16 2016
 
 @author: Pratik
 """
-from ftplib import FTP
-import os
 
-# login and download file from ftp site and retrieve file (use default params)
-def ftpDownloader(filename, host="ftp.pyclass.com", user="student@pyclass.com", passwd="student123"):
-    ftp = FTP(host)                 # get the host url of ftp site
-    ftp.login(user, passwd)         # login with username and password
-    ftp.cwd('Data')                 # change directory to Data
-    os.chdir("/Users/Pratik/Documents/Pratik/Work/practice/py-data-analysis") # change directory
-    print(ftp.nlst())               # print list of all files in dir
-    with open(filename, 'wb') as file:     # open file and w/r
-        ftp.retrbinary('RETR %s' % filename, file.write) # read contents of pdf and write to our file
+import os
+from ftplib import FTP, error_perm
+
+def ftpDownloader(stationId, startYear, endYear, url="ftp.pyclass.com", user="student@pyclass.com", passwd="student123", directory="/Users/Pratik/Documents/Pratik/Work/practice/py-data-analysis"):
+             ftp=FTP(url)
+             ftp.login(user, passwd)
+             if not os.path.exists(directory):    # if filepath doesn't exist create it and change dir
+                 os.makedirs(directory)
+             os.chdir(directory)
+             for year in range(startYear, endYear+1): # loop through directory 
+                 fullpath='/Data/%s/%s-%s.gz' % (year, stationId, year) # get file name
+                 filename=os.path.basename(fullpath)                    # get file name and ext only
+                 try:
+                     with open(filename, 'wb') as file:
+                         ftp.retrbinary("RETR %s" % fullpath, file.write)   # create new file and download
+                         print("%s successfully downloaded" % filename) 
+                 except error_perm:                                          # if file does not exist remove it
+                     print("%s is not available" % filename)
+                     os.remove(filename)
+             ftp.close()                                                # close connnection
+                     
