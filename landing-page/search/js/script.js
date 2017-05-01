0ccc819ebbdc89219763d796928519df4da871be
Pratik Shah <pratik.t.shah@gmail.com>
2017-05-01 17:36:49 -0500

    Add apps menu and style

--- landing-page/search/js/script.js
+++ landing-page/search/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+  pageLoad();
   var SAY = "I'm Feeling ";
   var phrase = ["Playful", "Hungry", "Puzzle", "Curious", "Artistic", "Happy"];
 
@@ -14,9 +14,47 @@ $(document).ready(function() {
     }
   );
 
+  // apps menu hide and then toggle
+  $(".apps-button").click(function() {
+    $("div#apps").toggle();
+  });
+
+  // apps menu edit
+  var iScrollPos = 0;
+
+	$("#apps").scroll(function () {
+    var iCurScrollPos = $(this).scrollTop();
+    if (iCurScrollPos > iScrollPos) {
+        //Scrolling Down
+        $('a[href="#more"]').hide();
+        $("hr").show();
+    } else {
+       //Scrolling Up
+       $('a[href="#more"]').show();
+       $("hr").hide();
+    }
+    iScrollPos = iCurScrollPos;
+	});
+
+  // scroll to second have of apps
+  $('a[href="#more"]').click(function(e) {
+    e.preventDefault();
+    var target = $("a#more").offset().top;
+    $("div#apps").animate(
+      {scrollTop: target},
+      500
+    );
+  });
+
 });
 
 // return random array index
 function random(arr) {
   return Math.floor(Math.random()*arr.length);
+}
+
+// page load setup
+function pageLoad() {
+  $("hr").hide();
+  $("div#apps").hide();
 }
